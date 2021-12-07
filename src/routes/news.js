e504133919a15f0b6b12c676b73b37fba30b76a2
Abdullatif <abdullatifk135@gmail.com>
2021-12-07 17:01:39 +0200

    news api app using nodejs and news api

---
+++ src/routes/news.js
@@ -0,0 +1,45 @@
+const express = require('express'); 
+const newsRouter = express.Router();
+const axios = require('axios');
+newsRouter.get("", async(req,res) => {
+    
+    try {
+        const newsAPI = await axios.get(`https://newsapi.org/v2/everything?q=technology&apiKey=""`);
+        res.render("news",{articles: newsAPI.data});
+    } catch (err) {
+        if(err.response){
+             console.log(err.response.data); 
+             console.log(err.response.status);
+             console.log(err.response.headers);
+        }
+        else if(err.requiest){
+            console.log(err.requiest);
+        }
+        else {
+             console.error("Error: ", err.message); 
+        }
+    }
+
+});
+newsRouter.post("", async(req,res) =>{
+      try {
+        const search = req.body.search;
+        const newsAPI = await axios.get(`https://newsapi.org/v2/everything?q=${search}&apiKey=eddc112fa3a04dc7b21ac4354fa91499`);
+        res.render("news",{articles: newsAPI.data});
+          
+      } catch (error) {
+        if(err.response){
+            console.log(err.response.data); 
+            console.log(err.response.status);
+            console.log(err.response.headers);
+       }
+       else if(err.requiest){
+           console.log(err.requiest);
+       }
+       else {
+            console.error("Error: ", err.message); 
+       }
+      }
+});
+
+module.exports = newsRouter;
